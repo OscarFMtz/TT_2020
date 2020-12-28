552f0683137cb3a0d9c8046d8a7d58ab5627d86d
OscarFMtz <39963593+OscarFMtz@users.noreply.github.com>
2020-12-28 17:45:02 -0600

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,10 @@
+
 import { Component } from '@angular/core';
+import { IonSlides, LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+import { UsuarioService } from '../usuario.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +12,67 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  @ViewChild(IonSlides) slides: IonSlides;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,        
+  };
+  constructor(
+              public alertCtrl: AlertController,
+              public loadingCtrl: LoadingController,
+              public _usuarioServ: UsuarioService,
+              private router: Router
+             ) {} 
+  
+  ionViewWillEnter(){        
+      this.slides.lockSwipes(true);    
+  }
+  
+  async mostrarInput(){
+    const alert = await this.alertCtrl.create({
+      header: 'Ingrese el usuario',
+      inputs: [{name: 'clave', placeholder: 'Introducir Clave'}],
+      buttons: [{text: 'Cancelar', role:'cancel'},
+                { text: 'Ingresar', handler: data => {
+                  //console.log(data);
+                  this.verificarUsuario(data.clave)
+                } 
+      }]
+    });
+     alert.present();
+  } 
 
-  constructor() {}
+  async verificarUsuario(clave: string){
+    let loading = await this.loadingCtrl.create({
+      message: 'Verificando...'
+    });
+    await  loading.present();
+    
+    this._usuarioServ.verificaUsuario(clave)
+            .then(async existe => {
+              loading.dismiss();
+              if(existe){
+                this.slides.lockSwipes(false);
+                this.slides.slideNext();
+                this.slides.lockSwipes(true);
+              }else{
+                (await this.alertCtrl.create({
+                  header: 'Usuario Incorrecto',
+                  subHeader: 'Hable con el administrador o intente de nuevo',
+                  buttons: ['Aceptar']
+                })).present();
+              }
+
+            })
+          
+  }
+
+  continuar(){
+    this.router.navigate(['/home']);
+    this.slides.lockSwipes(false);
+    this.slides.slidePrev();
+    this.slides.lockSwipes(true);    
+  }
+  
 
 }
