3bf328513108edf5276dc9107e8de28e452622f1
OscarFMtz <39963593+OscarFMtz@users.noreply.github.com>
2020-12-28 18:04:47 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { UsuarioService } from './usuario.service';
+import { Tab3Page } from '';
+import { MapsPage } from '';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +13,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  
+  rootPage: any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public _usuarioServe: UsuarioService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this._usuarioServe.cargarStorage().then( existe => {
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
+        if(existe){
+          this.rootPage = HomePage;
+        }else{
+          this.rootPage = Tab3Page;
+        }
+      
+      });
+      
     });
   }
+  
 }
